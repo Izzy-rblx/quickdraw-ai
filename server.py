6c3d4a651ee14a506406be897ed64222eac52edd
Izzy-rblx <grizzyrblx@gmail.com>
2025-09-08 19:45:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -57,4 +57,6 @@ def home():
     return "QuickDraw AI server is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
